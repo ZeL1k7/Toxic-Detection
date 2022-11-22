8ad5990639bf42c06d693bd9c17ce42c347ce704
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2022-11-22 16:36:39 +0300

    added typing and docstrings

--- predict.py
+++ predict.py
@@ -1,51 +1,53 @@
+from functools import lru_cache
 import json
+from pathlib import Path
 import torch
 import torch.nn.functional as F
 from flask_restful import Resource
-from functools import lru_cache
 from utils import load_toxic_model
 
-class ToxicClassifer(Resource):
 
-    def __init__(self,model):
+class ToxicClassifer(Resource):
+    """
+    Потом напишу :)
+    """
+    def __init__(self):
         self.model = self.make_pt_model('state_dict.pt')
         self.output_filepath = 'predictions.json'
-    @lru_cache(1)  # загружаем модель + кэшируем ее
-    def make_pt_model(self,model_path):
+
+    @lru_cache(1)
+    def make_pt_model(self, model_path: Path) -> torch.nn.Module:
+        """
+        Загрузка и кэширование предобученной модели
+        :param model_path: путь до state_dict предобученной модели
+        :return: torch.nn.Module: предобученная модель
+        """
         print('Started loading model')
         model = load_toxic_model(model_path)
         print('Successful loaded model')
         return model
 
-    def get(self, filepath):
+    def predict_from_json(self, path: Path) -> Path:
+        """
+        Inference модели
+        :param path: путь до json с исходными данными
+        :return: output_filepath: путь до json с результатами модели
+        """
+        # получаем на вход json,в котором содержится наш текст
+        with open(path) as file:
+            data = json.load(file)
+        text_list = data['text']
 
-        try:
-            with open (filepath) as file:  # получаем на вход json,в котором содержится наш текст
-                data = json.load(file)
-            text_list = data['text']
+        logits = []
+        probs = []
 
-            logits = []
-            probs = []
-
-            with torch.no_grad():
-                for text in text_list:
-                    logits.append(self.model(text).item())
-                    probs.append(F.sigmoid(logits).item())
-
-            with open(self.output_filepath, 'w+') as f:  # сохраняем наши предсказания в json file
-                json.dump({'logits': logits, 'probabilities': probs}, f)
-
-            return self.output_filepath, 200  # возвращаем названия json в случае успешного выполнения
-        except:
-            'error', 400
-
-'''
-TO DO!!!
-
-from pedantic import BaseModel
-
-class Item(BaseModel):
-id: int = Field(..., description="Unique item id")
-text: str = Field(..., description="какой то текст который ты там передаешь?")
-
-'''
+        with torch.no_grad():
+            for text in text_list:
+                logits.append(self.model(text).item())
+                probs.append(F.sigmoid(logits).item())
+        # сохраняем наши предсказания в json file
+        with open(self.output_filepath, 'w+') as file:
+            json.dump({'logits': logits, 'probabilities': probs}, file)
+        # возвращаем названия json в случае успешного выполнения
+        return self.output_filepath
+    
