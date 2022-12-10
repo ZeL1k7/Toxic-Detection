18cc2e955cc1f351dbbcf32a2a3dd8ddbf728f2f
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2022-12-10 17:26:42 +0300

    added encrtypting

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, render_template, request
 from predict import ToxicClassifer
+from utils import DHExchange
+import json
+
 
 app = Flask(__name__, template_folder='.')
-api = Api(app)
 
 
-api.add_resource(ToxicClassifer, "/toxic")
+@app.route('/', methods=['GET'])
+def index():
+    query = request.args.get('query')
+    if query is None:
+        query = ''
+        answer = ['None', 'NaN']
+    else:
+        clf = ToxicClassifer()
+        c_public = 197
+        c_private = 199
+        s_public = 151
+        client = DHExchange(c_public, s_public, c_private)
+        s_partial = client.generate_partial_key()
+        c_full = client.generate_full_key(clf.get_client_partial_key(s_partial))
+        filename = 'query.json'
+        with open(filename, 'w+') as f:
+            json.dump(query, f)
+        filename_encrypted = client.encrypt_message(filename)
+        filepath = clf.predict_from_json(s_partial, filename_encrypted)
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+        answer = ('toxic', float(data['probabilities'])) if data['probabilities'] > 0.5 \
+            else ('not toxic', 1-float(data['probabilities']))
+
+    return render_template(
+        'templates/index.html',
+        answer=answer[0],
+        query=query,
+        probs=answer[1]
+    )
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- predict.py
+++ predict.py
@@ -3,14 +3,18 @@ import json
 from pathlib import Path
 import torch
 import torch.nn.functional as F
-from flask_restful import Resource
-from utils import load_toxic_model
+from utils import load_toxic_model, DHExchange
 
 
-class ToxicClassifer(Resource):
+class ToxicClassifer:
     def __init__(self):
         self.model = self.make_pt_model('state_dict.pt')
         self.output_filepath = 'predictions.json'
+        c_public = 197
+        s_public = 151
+        s_private = 157
+        self.server = DHExchange(c_public, s_public, s_private)
+        #s_full = server.generate_full_key(s_partial)
 
     @lru_cache(1)
     def make_pt_model(self, model_path: Path) -> torch.nn.Module:
@@ -24,13 +28,21 @@ class ToxicClassifer(Resource):
         print('Successful loaded model')
         return model
 
-    def predict_from_json(self, path: Path) -> Path:
+    def get_client_partial_key(self, s_partial):
+        return self.server.generate_partial_key()
+
+
+    def predict_from_json(self, c_partial, path: Path) -> Path:
         """
         Inference модели
         :param path: путь до json с исходными данными
         :return: output_filepath: путь до json с результатами модели
         """
         # получаем на вход json,в котором содержится наш текст
+
+        s_full = self.server.generate_full_key(c_partial)
+        path = self.server.decrypt_message(path)
+
         with open(path) as file:
             data = json.load(file)
         text_list = data['text']
@@ -45,4 +57,4 @@ class ToxicClassifer(Resource):
         # сохраняем наши предсказания в json file
         with open(self.output_filepath, 'w+') as file:
             json.dump({'logits': logits, 'probabilities': probs}, file)
-        return self.output_filepath
+        return  self.output_filepath
